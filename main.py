372ff8ff44da16ca2a7ea62d32b3d9eea55b1932
giuliaRaffaellaVitale <giuliavitale611@gmail.com>
2026-02-02 11:07:30 +0100

    added the main and adapted the OMS class

---
+++ main.py
@@ -0,0 +1,10 @@
+from Filtering_techniques.OMSSaliencyMapFiltering import OMSFiltering
+
+if __name__ == "__main__":
+    
+    # Initialize and run OMS Filtering
+    filter = OMSFiltering("DVSGesture")
+
+    filter.OMS_filtering()
+
+    
