182107f8224796b1e08a1e45e8857eb8e2b67fd5
giuliaRaffaellaVitale <giuliavitale611@gmail.com>
2026-02-02 16:11:00 +0100

    adapted saliencyCropping

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Filtering_techniques.OMSSaliencyMapFiltering import OMSFiltering
 from Filtering_techniques.MaskAdaptiveElbow  import AdaptiveElbowOMSFiltering
 from Filtering_techniques.MaskGoalOriented import MaskGoalOrientedOMSFiltering
 from Filtering_techniques.MaskMeanStandardDeviation import MaskMeanStandardDeviation
+from Filtering_techniques.MaskGlobalSaliencyBasedCropping import MaskGlobalSaliencyBasedCropping
 
 if __name__ == "__main__":
     
@@ -25,10 +26,19 @@ if __name__ == "__main__":
     filtered_events, masked_OMS, OMSMap = GoalOrientedFilter.Goadaptive_thresholding(keep_percent) 
     GoalOrientedFilter.GoalOriented_filtering_visualization(OMSMap, masked_OMS)
     """
-
+    """
     # Initialize and run Mean and Standard Deviation Thresholding
     MeanStdFilter = MaskMeanStandardDeviation("DVSGesture")
     k_sigma = 2.0  # Parameter for thresholding
     filtered_events = MeanStdFilter.Mean_std_thresholding(k_sigma)
     MeanStdFilter.MeanStd_filtering_visualization(filtered_events, k_sigma)
+    """
+
+    # Initialize and run Global Saliency Based Cropping
+    GlobalSaliencyCropper = MaskGlobalSaliencyBasedCropping("DVSGesture")
+    Use_percentile = True
+    percentile = 90
+    threshold = 0.4
+    filred_events, OMS_norm, cropped_OMS_map, crop_box = GlobalSaliencyCropper.MaskGlobalSaliency_filtering(Use_percentile, percentile, threshold)
+    GlobalSaliencyCropper.MaskGlobalSaliency_filtering_visualization(cropped_OMS_map, OMS_norm)
 
